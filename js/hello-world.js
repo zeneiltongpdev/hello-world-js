d07eeb4a854a2a49797b5db10adce23e1427dfd8
✞Ȥɘɳɘʅ✞ <zeneiltongranja@gmail.com>
2023-07-14 02:16:37 -0300

    up-to-date files v2

--- js/hello-world.js
+++ js/hello-world.js
@@ -3,5 +3,8 @@ const botao = document.querySelector("#botao");
 botao.addEventListener("click", exibirAlerta);
 
 function exibirAlerta() {
-    alert("Hello World!");
+  swal("Hello World!", "Cubes Academy - First Exercise", "success", {
+    buttons: false,
+    timer: 2800,
+  });
 }
