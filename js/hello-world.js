643369e467b45560811124a67bb9463996cde176
Zeneilton Granja de Paulo <zeneiltongranja@gmail.com>
2023-07-14 14:20:18 -0300

    Update hello-world.js
    Signed-off-by: Zeneilton Granja de Paulo <zeneiltongranja@gmail.com>

--- js/hello-world.js
+++ js/hello-world.js
@@ -3,7 +3,7 @@ const botao = document.querySelector("#botao");
 botao.addEventListener("click", exibirAlerta);
 
 function exibirAlerta() {
-  swal("Hello World!", "Cubes Academy - First Exercise", "success", {
+  swal("Hello World!", "Cubos Academy - First Exercise", "success", {
     buttons: false,
     timer: 2800,
   });
